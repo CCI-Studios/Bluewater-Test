b954ef9b4622b25164a8d282b0a04c75e1304bb5
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-22 08:36:04 -0400

    Added template to extensions list

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,4 +7,4 @@ role :app, "ccistaging.com"
 # directories
 set :deploy_to, "/home/staging/subdomains/bwhf"
 set :public, "#{deploy_to}/public_html"
-set :extensions, %w[com_donate plg_ie6]
+set :extensions, %w[com_donate plg_ie6 template]
