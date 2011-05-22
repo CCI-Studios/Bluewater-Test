1dc270a4acc89a3a0f04a43a126b4d2364079009
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-22 08:16:42 -0400

    Template crap

---
+++ template/templates/bluewater/index.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<?php
+error_reporting(0);
+?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Bluewater Health Foundation - Who Are We?</title> 
+	<meta name="title" content="Who Are We?" /> 
+	<meta name="author" content="Administrator" /> 
+	<meta name="description" content="Bluewater Health Hospital Charitable Foundation" /> 
+	<meta name="keywords" content="bluewater health, foundation, charity, dream home lottery, lifeline, lambton, sarnia, hospital" /> 
+	<meta name="Generator" content="Mambo - Copyright 2000 - 2006 Mambo Foundation.  All rights reserved." /> 
+	<meta name="robots" content="index, follow" /> 
+	<style>/* Styles for the img box */
+	div.mtImgBoxStyle {
+	 margin-left:5px;
+	 margin-right:5px; 
+	} 
+	/* Styles for the caption */
+	div.mtCapStyle {
+	 font-weight: bold;
+	 color: black;
+	 background-color: #ddd;
+	 padding: 2px;
+	 text-align:center;
+	 overflow:hidden; 
+	}</style> 
+	<link rel="shortcut icon" href="http://www.bluewaterhealthfoundation.ca/images/favicon.ico" /> 
+	<link rel="stylesheet" type="text/css" href="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/css/template_css.css">
+	<link rel="stylesheet" type="text/css" href="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/css/top.css">
+	<link rel="stylesheet" type="text/css" href="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/css/left.css">
+	<link rel="stylesheet" type="text/css" href="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/css/main.css">
+	<link rel="stylesheet" type="text/css" href="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/css/max.css">
+	<link rel="shortcut icon" href="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/favicon.ico" />
+	<script language="javascript" type="text/javascript" src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/javascript/leftframe.js"></script>
+	<style>
+			.pollstableborder {
+			border: 0px solid;
+			padding: 0px;
+			}
+	</style>
+	<title>Bluewater Health Foundation</title>
+	
+	<link rel="stylesheet" type="text/css" href="/templates/bluewater/css/template.css" />
+</head>
+<script language="javascript">
+<!--
+	function setH() {
+		var s = document.getElementById ('stick');
+		var y;
+		if (self.innerHeight) {// all except Explorer
+			y = self.innerHeight;
+		} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+			y = document.documentElement.clientHeight;
+		} else if (document.body) { // other Explorers
+			y = document.body.clientHeight;
+		}
+		s.style.height = (y-100) + 'px';
+	}
+
+//-->
+</script>
+<body background="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/max_page_bg.jpg" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="100%" height="90" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td valign="middle" width="763">
+    <img src="http://www.bluewaterhealthfoundation.ca/images/stories/FrontImages/logo_foundation.gif" align="left"  hspace="55" alt="logo_foundation.gif" />
+    <a href="http://www.lifeline.ca" target="_blank"> <img src="http://www.bluewaterhealthfoundation.ca/images/stories/FrontImages/logo_lifeline2.gif" align="right"  hspace="0" alt="bhf logo 150 .jpg" /> </a>
+  </tr>
+</table>
+
+<table width="100%" height="25" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+  <td bgcolor="#FFFFFF">&nbsp;
+  <b>Serving the three hospital sites of Lambton County</b>
+  </td>
+  <td align="right" bgcolor="#FFFFFF">
+  <b>Sunday, 22 May 2011</b>&nbsp;</td>
+  </tr>
+</table>
+
+<!--
+<table width="100%" height="25" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+  <td bgcolor="#FFFFFF">&nbsp;-&nbsp; 
+    <span class="pathway"><a href="index.php" class="pathway">Home</a> <img src="http://www.bluewaterhealthfoundation.ca/images/M_images/arrow.png" alt="arrow" />   About Us </span>
+  </td></tr>
+</table>
+-->
+
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr> 
+    <td width="212" valign="top" background="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/max_left_bar.jpg"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td class="left"  valign="top"> 
+            		<table cellspacing="0" cellpadding="0" align="center" class="navOpened" width="100%" id="sw_n0">
+			  			<tr>
+    			<th valign="top"  onClick="opentree('sw_n0')"><table border="0" cellspacing="0" cellpadding="0" width="100%" class="navTitle" onMouseOver="mover(this)" onMouseOut="mout(this)">
+	<tr>
+		<td class="titleLeft"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" border="0" alt="" valign="top" width="14" heigth="1"/></td>
+		<td width="100%" class="titleText">Main Menu</td>
+		<td class="titleHandle"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" border="0" alt="" valign="top" width="20" heigth="1"/></td>
+		<td class="titleRight"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" border="0" alt="" valign="top" width="3" heigth="1"/></td>
+	</tr>
+</table>
+</th>
+    		</tr>
+			    		<tr>
+    			<td class="modulecontent"><div class="modulecontent">
+			
+<table width="100%" border="0" cellpadding="0" cellspacing="0"> 
+<tr align="left"><td><a href="index.php?option=com_frontpage&amp;Itemid=1" class="mainlevel" >Home</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=20&amp;Itemid=35" class="mainlevel" id="active_menu">About Us</a> 
+ 
+<div style="padding-left: 4px"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/indent1.png" alt="" /><a href="index.php?option=com_content&amp;task=view&amp;id=23&amp;Itemid=39" class="sublevel" >Board of Directors</a></div> 
+<div style="padding-left: 4px"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/indent1.png" alt="" /><a href="index.php?option=com_content&amp;task=view&amp;id=24&amp;Itemid=40" class="sublevel" >Bluewater Health Auxiliary</a></div> 
+<div style="padding-left: 4px"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/indent1.png" alt="" /><a href="index.php?option=com_content&amp;task=view&amp;id=228&amp;Itemid=74" class="sublevel" >Children's Auxiliary</a></div> 
+</td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=21&amp;Itemid=50" class="mainlevel" >How To Give</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=19&amp;Itemid=34" class="mainlevel" >Donor Information</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=282&amp;Itemid=77" class="mainlevel" >Current Needs</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=22&amp;Itemid=37" class="mainlevel" >Special Events</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_events&amp;Itemid=28" class="mainlevel" >Events Calendar</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=section&amp;id=1&amp;Itemid=2" class="mainlevel" >News and Info</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=187&amp;Itemid=70" class="mainlevel" >Employment Opportunities</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=category&amp;sectionid=3&amp;id=30&amp;Itemid=51" class="mainlevel" >FAQ's</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=114&amp;Itemid=62" class="mainlevel" >Useful Links</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_contact&amp;Itemid=3" class="mainlevel" >Contact Us</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_donate&amp;view=transaction&amp;Itemid=2" class="mainlevel" >MAKE A DONATION!</a></td></tr> 
+<tr align="left"><td><span class="mainlevel" >- - - - - - -</span></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=237&amp;Itemid=75" class="mainlevel" >DREAM HOME LOTTERY</a></td></tr> 
+<tr align="left"><td><a href="http://www.lifeline.ca" target="_blank" class="mainlevel" >Philips Lifeline</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_content&amp;task=view&amp;id=53&amp;Itemid=66" class="mainlevel" >State of the Heart Campaign</a></td></tr> 
+<tr align="left"><td><a href="http://www.bluewaterhealth.ca" target="_blank" class="mainlevel" >Bluewater Health</a></td></tr> 
+<tr align="left"><td><a href="index.php?option=com_contact&amp;task=view&amp;contact_id=5&amp;Itemid=79" class="mainlevel" >Charity Ball</a></td></tr> 
+</table>    			</div></td>
+			</tr>
+      	</table>
+		
+			<table cellspacing="0" cellpadding="0" align="center" class="navOpened" width="100%" id="sw_n1">
+			    		<tr>
+    			<td class="modulecontent"><div class="modulecontent">
+			
+<form action="index.php" method="post"> 
+ 
+<div align="left" class="search">	
+<input alt="search" class="inputbox" type="text" name="searchword" size="20" value="search..."  onblur="if(this.value=='') this.value='search...';" onfocus="if(this.value=='search...') this.value='';" /></div> 
+ 
+<input type="hidden" name="option" value="search" /> 
+</form>    			</div></td>
+			</tr>
+      	</table>
+		
+	          </td>
+          <script language="javascript">
+ <!--
+	var y;
+	if (self.innerHeight) {// all except Explorer
+		y = self.innerHeight;
+	} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+		y = document.documentElement.clientHeight;
+	} else if (document.body) { // other Explorers
+		y = document.body.clientHeight;
+	}
+	document.write ('<td id="stick" style="height: '+(y-100)+'px; width: 1px; background-color: #cbcbd5" valign="top"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" width="1" height="1" border="0" alt=""/></td>');
+
+ //-->
+ </script>
+        </tr>
+      </table></td>
+    <td width="1" bgcolor="#FFFFFF">&nbsp;</td>
+    <td class="main" valign="top" bgcolor="#FFFFFF"> <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td valign="top" class="maincontent"> 
+				<jdoc:include type="component" />				
+			</td>
+        </tr>
+        <tr> 
+          <td height="15"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" width="1" height="1"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" width="1" height="1"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" width="1" height="1"></td>
+        </tr>
+        <tr> 
+          <td height="15"> 
+                      </td>
+        </tr>
+        <tr> 
+          <td height="15"> 
+                      </td>
+        </tr>
+      </table></td>
+    <td width="1" bgcolor="#FFFFFF">&nbsp;</td>
+    <td width="190" valign="top" bgcolor="#FFFFFF"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td  width="190" valign="top" class="right"> 
+                        		<table cellspacing="0" cellpadding="0" align="center" class="moduletable" width="100%">
+			  			<tr>
+    			<th valign="top" ><table width="100%" cellspacing="0" cellpadding="0" border="0">
+	<tr>
+		<td class="moduleheaderleft2"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/1x1.gif" width="3" height="24" border="0" alt=""/></td>
+		<td class="xyz">Upcoming Events</td>
+	</tr>
+</table></th>
+    		</tr>
+			    		<tr>
+    			<td class="modulecontent"><div class="modulecontent">
+			
+<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center"><tr><td class='mod_events_latest_noevents'>No Latest Events</td></tr> 
+</table> 
+    			</div></td>
+			</tr>
+      	</table>
+		
+	                      </td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td colspan="2" background="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/max_foot_line.jpg"><img src="http://www.bluewaterhealthfoundation.ca/templates/xp.winupdate.like/images/max_foot_line.jpg" width="1" height="1"></td>
+  </tr>
+  <tr> 
+    <td>&nbsp;</td>
+    <td align="right"> <font color="#FFFFFF" size="+1" face="Verdana, Arial, Helvetica, sans-serif"><strong><em>Bluewater Health Foundation</em></strong></font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
+<!-- 1306065473 -->="http://www.bluewaterhealthfoundation.ca/template
